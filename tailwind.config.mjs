6c4b6bbb7db5b165e9df98a01189118aa7a7524e
Delio Yunior <yunior0716@gmail.com>
2025-07-24 04:59:39 -0400

    v2 - updated information and UI

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          text: 'rgb(var(--text-primary))',
+        },
+        secondary: {
+          text: 'rgb(var(--text-secondary))',
+        },
+        accent: 'rgb(var(--accent))',
+      },
+    },
   },
   plugins: [],
 };
